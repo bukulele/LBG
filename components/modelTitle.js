dc05a5804f77a7a71673842e1fb633a2f777e9ec
bukulele <zonough@gmail.com>
2023-05-23 17:32:38 +0400

    model title structure added

--- components/modelTitle.js
+++ components/modelTitle.js
@@ -6,7 +6,18 @@ class modelTitle extends HTMLElement {
   connectedCallback() {
     this.attachShadow({mode: 'open'});
     this.shadowRoot.innerHTML = `
-    <slot name="model-title-image"></slot>`;
+    <a class="model-title__container">
+      <slot name="model-title-image"></slot>
+      <div class="model-title__block-over">
+        <div class="model-title__labels"></div>
+        <div class="model-title__games"></div>
+        <div class="model-title__punishments"></div>
+      </div>
+      <div class="model-title__info">
+      <div class="model-title__model-name"></div>
+      <div class="model-title__model-age"></div>
+      </div>
+    </a>`;
   }
 }
 
