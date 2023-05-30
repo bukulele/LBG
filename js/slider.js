e8b55a83d858d3f51d971f974cfc95b99947759b
bukulele <zonough@gmail.com>
2023-05-30 19:18:59 +0400

    placing of slider corrected, move left to bu corrected

--- js/slider.js
+++ js/slider.js
@@ -1,25 +1,31 @@
 // let sliderImages = document.querySelector('.slider-block__slider');
 const startingElementNumber = 2;
+
+let sliderElementReference = document.querySelector("#page-container > div > videos-block > video-title:nth-child(1)").shadowRoot.querySelector(".video-title__container");
+let sliderElementAll = document.querySelectorAll('#page-container > div > slider-block > slider-element');
+for (let elem of sliderElementAll) {
+  elem.shadowRoot.querySelector(".slider-element__container").style.width = `${sliderElementReference.offsetWidth}px`;
+}
+
 let sliderBlockContainer = document.querySelector('#page-container > div > slider-block').shadowRoot.querySelector('.slider-block__container');
 let sliderImages = document.querySelector("#page-container > div > slider-block").shadowRoot.querySelector(".slider-block__slider");
 let gapBetweenSlides = Number(window.getComputedStyle(sliderImages).gap.slice(0, -2));
 let numberOfSlides = sliderImages.children.length;
 let currentSlide = startingElementNumber;
-
-let windowWidth = window.innerWidth;
-let sliderElement_2 = document.querySelector(`#page-container > div > slider-block > slider-element:nth-child(${startingElementNumber + 1})`).shadowRoot.querySelector(".slider-element__container");
+let windowWidth = document.body.clientWidth;
+let sliderElement_1 = document.querySelector(`#page-container > div > slider-block > slider-element:nth-child(${startingElementNumber})`).shadowRoot.querySelector(".slider-element__container");
 let sliderElement_butOne = document.querySelector(`#page-container > div > slider-block > slider-element:nth-child(${numberOfSlides})`).shadowRoot.querySelector(".slider-element__container");
-let initialPosition = (windowWidth - sliderElement_2.offsetWidth) / 2 - sliderElement_2.offsetLeft;
-let butOnePosition = (windowWidth - sliderElement_2.offsetWidth) / 2 - sliderElement_butOne.offsetLeft;
-
-sliderImages.style.left = `${initialPosition}px`;
-
+let initialPosition = (windowWidth) / 2 - sliderElement_1.offsetWidth - gapBetweenSlides * 2 - sliderElement_1.offsetWidth / 2 ;
+let butOnePosition = (windowWidth) / 2 - sliderElement_butOne.offsetWidth - gapBetweenSlides * 2 - sliderElement_butOne.offsetWidth / 2 - sliderElement_butOne.offsetLeft;
+// let butOnePosition = (windowWidth - sliderElement_butOne.offsetWidth) / 2 - sliderElement_butOne.offsetLeft;
 let buttonLeft = sliderBlockContainer.querySelector('.slider-block__slide-left');
 let buttonRight = sliderBlockContainer.querySelector('.slider-block__slide-right');
+sliderImages.style.left = `${initialPosition}px`;
+console.log(window.innerWidth);
+console.log(document);
 
 buttonLeft.addEventListener('click', moveSlidesLeft);
 buttonRight.addEventListener('click', moveSlidesRight);
-
 function moveSlidesLeft () {
   addAnimation(sliderImages);
   if (currentSlide === 2) {
@@ -28,25 +34,25 @@ function moveSlidesLeft () {
     currentSlide = numberOfSlides - 2;
     setTimeout(() => {
       addAnimation(sliderImages);
-      sliderImages.style.left = `${sliderImages.offsetLeft + sliderElement_2.offsetWidth + gapBetweenSlides}px`;
+      sliderImages.style.left = `${sliderImages.offsetLeft + sliderElement_1.offsetWidth + gapBetweenSlides * 2}px`;
     }, 0);
   } else {
     currentSlide -= 1;
-    sliderImages.style.left = `${sliderImages.offsetLeft + sliderElement_2.offsetWidth + gapBetweenSlides}px`;
+    sliderImages.style.left = `${sliderImages.offsetLeft + sliderElement_1.offsetWidth + gapBetweenSlides}px`;
   }
 }
 function moveSlidesRight () {
   addAnimation(sliderImages);
   if (currentSlide + 2 === numberOfSlides) {
     currentSlide = 2;
-    sliderImages.style.left = `${sliderImages.offsetLeft - sliderElement_2.offsetWidth - gapBetweenSlides}px`;
+    sliderImages.style.left = `${sliderImages.offsetLeft - sliderElement_1.offsetWidth - gapBetweenSlides}px`;
     setTimeout(() => {
       removeAnimation(sliderImages);
       sliderImages.style.left = `${initialPosition}px`;
       }, 200);
   } else {
     currentSlide += 1;
-    sliderImages.style.left = `${sliderImages.offsetLeft - sliderElement_2.offsetWidth - gapBetweenSlides}px`;
+    sliderImages.style.left = `${sliderImages.offsetLeft - sliderElement_1.offsetWidth - gapBetweenSlides}px`;
   }
 }
 
