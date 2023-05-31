ae867294960a89a34945ac8919bbc131501949cd
bukulele <zonough@gmail.com>
2023-05-31 21:23:06 +0400

    new project structure using webpack instead of webComponents

---
+++ src/components/sliderBlock.js
@@ -0,0 +1,21 @@
+import image1 from '../../assets/temp/image 1.png';
+import image2 from '../../assets/temp/image 2.png';
+import image3 from '../../assets/temp/image 3.png';
+import image4 from '../../assets/temp/image 4.png';
+import image5 from '../../assets/temp/image 5.png';
+import image6 from '../../assets/temp/image 6.png';
+import sliderElement from './sliderElement.html';
+
+const videos = [image1, image2, image3, image4, image5, image6, image1, image2, image3];
+
+let sliderBlockSlider = document.getElementById('videosBlockTitles');
+
+for (let video of videos) {
+  videosBlockTitles.insertAdjacentHTML('beforeend', videoTitle);
+  let imageElement = document.createElement('img');
+  imageElement.src = video;
+  let elem = videosBlockTitles.lastElementChild.querySelector('.model-title__image')
+  title.insertAdjacentElement('afterbegin', imageElement);
+  videosBlockTitles.lastElementChild.insertAdjacentElement('afterbegin', imageElement);
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/main.js',
+    videosBlock: './src/components/videosBlock.js',
+    modelsBlock: './src/components/modelsBlock.js',
+  }, // The entry point of your application
+  output: {
+    path: path.resolve(__dirname, 'dist'), // The output directory
+    filename: '[name].bundle.js', // The bundled JavaScript file
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/, // Process HTML files
+        use: ['html-loader']
+      },
+      {
+        test: /\.css$/, // Process CSS files
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]'
+        }
+      }
+    ]
+  }
+};
