e89d1999a25a375d64bdea667fcaa844448ef494
bukulele <zonough@gmail.com>
2023-06-13 13:35:30 +0400

    videos filter component separated

--- src/components/videosBlockPage.js
+++ src/components/videosBlockPage.js
@@ -7,10 +7,13 @@ import image6 from '../../assets/temp/image 6.png';
 import videoTitle from './videoTitle.html';
 import banner2 from './banner2.html';
 import messageBlock from './messageBlock.html';
+import filters from './videosFilters.html';
 
 const videos = [image1, image2, image3, image4, image5, image6];
 
 let videosBlockTitles = document.getElementById('videosBlockTitles');
+
+videosBlockTitles.insertAdjacentHTML('beforebegin', filters);
 
 for (let video of videos) {
   videosBlockTitles.insertAdjacentHTML('beforeend', videoTitle);
