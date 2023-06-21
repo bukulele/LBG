9e24858fc7b2c34bbaf4fc8eaaefc65df6a246fa
bukulele <zonough@gmail.com>
2023-06-21 14:37:41 +0400

    working on comments block

--- src/videoPage.js
+++ src/videoPage.js
@@ -5,12 +5,14 @@ import banner from './components/banner.html';
 import timerBlock from './components/timerBlock.html';
 import picturesBlock from './components/picturesBlock.html';
 import watchVideoBlock from './components/watchVideoBlock.html';
+import commentsBlock from './components/commentsBlock.html';
 
 document.getElementById('navbar').innerHTML = navbar;
 document.getElementById('footer').innerHTML = footer;
 document.getElementById('watchVideoBlock').innerHTML = watchVideoBlock;
 document.getElementById('picturesBlock').innerHTML = picturesBlock;
 document.getElementById('videosBlock').innerHTML = videosBlock;
+document.getElementById('commentsBlock').innerHTML = commentsBlock;
 document.getElementById('banner_1').innerHTML = banner;
 document.getElementById('timerBlock').innerHTML = timerBlock;
 
