e75df90ea3b10bf86ec0cd64537ceeb3b1e4c636
bukulele <zonough@gmail.com>
2023-07-05 13:37:03 +0400

    working on terms block

--- src/termsPage.js
+++ src/termsPage.js
@@ -1,13 +1,11 @@
 import navbar from './components/navbar.html';
 import footer from './components/footer.html';
-import oneNewsBlock from './components/oneNewsBlock.html';
+import termsBlock from './components/termsBlock.html';
 import banner from "./components/banner.html";
 import timerBlock from "./components/timerBlock.html";
-import commentsBlock from './components/commentsBlock.html';
 
 document.getElementById('navbar').innerHTML = navbar;
 document.getElementById('footer').innerHTML = footer;
-document.getElementById('newsBlock').innerHTML = oneNewsBlock;
+document.getElementById('termsBlock').innerHTML = termsBlock;
 document.getElementById('banner_1').innerHTML = banner;
 document.getElementById('timerBlock').innerHTML = timerBlock;
-document.getElementById('commentsBlock').innerHTML = commentsBlock;
