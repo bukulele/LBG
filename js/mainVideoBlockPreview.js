4f893ad116db8f17ca49abbc5dfba6ac885435cd
bukulele <zonough@gmail.com>
2023-08-01 19:04:38 +0400

    testing observing the main image loading

--- js/mainVideoBlockPreview.js
+++ js/mainVideoBlockPreview.js
@@ -1,21 +1,28 @@
 let mainImage = document.querySelector('.main-video-block__image img');
 
-let options = {
-  root: document.querySelector(".main-video-block__container"),
-  rootMargin: "0px",
-  threshold: 1.0,
-};
-
-let observer = new IntersectionObserver(handleImageLoading, options);
-
-observer.observe(mainImage);
-
-function handleImageLoading (entries, observer) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      console.log('IMAGE LOADED');
-    }
-  })
-  console.log(entries);
-  console.log(observer);
+function handleImageLoading () {
+  console.log("IMAGE LOADED");
 }
+
+mainImage.addEventListener("load", handleImageLoading);
+
+//
+// let options = {
+//   root: document.querySelector(".main-video-block__container"),
+//   rootMargin: "0px",
+//   threshold: 1.0,
+// };
+//
+// let observer = new IntersectionObserver(handleImageLoading, options);
+//
+// observer.observe(mainImage);
+//
+// function handleImageLoading (entries, observer) {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       console.log('IMAGE LOADED');
+//     }
+//   })
+//   console.log(entries);
+//   console.log(observer);
+// }
