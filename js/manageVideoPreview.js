275add8e5cd506e43c71a083887dafc35c8a4963
bukulele <zonough@gmail.com>
2023-08-03 16:42:38 +0400

    video preview unnecessary comments removed

--- js/manageVideoPreview.js
+++ js/manageVideoPreview.js
@@ -1,42 +1,27 @@
-// var videosPreviewContainer = document.querySelector('#videosBlock .videos-block__container');
-// let videoTimerId;
 let currentTarget = null;
 
 function showLoader(event) {
   if (event.target.className.includes('video-title__container')) {
-    
+
     if (currentTarget) return;
-
     currentTarget = event.target;
-    event.target.classList.add('preview-loading');
-    // videoTimerId = setTimeout(() => {
-      let videoElement = event.target.querySelector('.video-title__video video');
-      videoElement.src = './dist/videos/097 - That\'s My Mommy (1955)_New.mov';
-    // videoElement.setAttribute('webkit-playsinline', '');
-    //   event.target.insertAdjacentElement('afterbegin', videoElement);
-    //   if (videoElement.pictureInPictureElement) {
-    //     videoElement.exitPictureInPicture();
-    //   }
-      videoElement.play()
-        .then(() => {
-          hideLoader(event.target);
-          event.target.querySelector('.video-title__image').style.display = 'none';
-          event.target.querySelector('.video-title__video').style.display = 'flex';
-        })
-        .catch(e => console.log(e));
-    // }, 1000);
 
-    // Prevent fullscreen when playing
-    // videoElement.addEventListener("webkitbeginfullscreen", function () {
-    //   if (!videoElement.webkitDisplayingFullscreen) {
-    //     videoElement.webkitEnterFullScreen();
-    //   }
-    // });
+    event.target.classList.add('preview-loading');
+
+    let videoElement = event.target.querySelector('.video-title__video video');
+    videoElement.src = './dist/videos/097 - That\'s My Mommy (1955)_New.mov';
+    videoElement.play()
+      .then(() => {
+        hideLoader(event.target);
+        event.target.querySelector('.video-title__image').style.display = 'none';
+        event.target.querySelector('.video-title__video').style.display = 'flex';
+      })
+      .catch(e => console.log(e));
   }
 }
 
 function hideLoader(target) {
-    target.classList.remove('preview-loading');
+  target.classList.remove('preview-loading');
 }
 
 function removeVideo(event) {
@@ -48,22 +33,14 @@ function removeVideo(event) {
 
       relatedTarget = relatedTarget.parentNode;
     }
-    // console.log('OUT');
+
     let videoElement = event.target.querySelector('.video-title__video video');
     videoElement.pause();
-    // videoElement.src='';
+
     event.target.querySelector('.video-title__image').style.display = 'flex';
     event.target.querySelector('.video-title__video').style.display = 'none';
-    // if (videoTimerId) {
-    //   clearTimeout(videoTimerId);
-    // }
-    // if (videoElement) {
-      // videoElement.removeEventListener('loadeddata');
-      // videoElement.remove();
-      // event.target.querySelector('img').style.visibility = 'visible';
-      // event.target.querySelector('.video-title__block-over').style.display = 'flex';
-    // }
     hideLoader(event.target);
+
     currentTarget = null;
   }
 }
