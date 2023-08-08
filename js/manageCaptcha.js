330b5fa6b734b34c96d066937bc7b06d33c090ba
bukulele <zonough@gmail.com>
2023-08-08 14:11:24 +0400

    recaptcha placed

--- js/manageCaptcha.js
+++ js/manageCaptcha.js
@@ -1,3 +1,11 @@
+function onloadCaptcha () {
+  grecaptcha.render('recaptchaContainer', {
+    'sitekey' : '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
+    'callback': passCaptcha
+  });
+
+}
+
 function passCaptcha () {
   console.log('YO CAPTCHA');
 }
