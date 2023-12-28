8e0000200fbe6df873191fcc3a6b70a43672edc7
bukulele <zonough@gmail.com>
2023-12-28 11:23:57 -0300

    showing the image gallery approach updated; switching gallery images from keyboard added

--- js/imagesGallery.js
+++ js/imagesGallery.js
@@ -10,27 +10,31 @@ function openImageGallery(event) {
     event.preventDefault();
     imagesList = JSON.parse(targetElement.getAttribute('data-gallery-images'));
     imageIndex = imagesList.findIndex(item => item === event.target.getAttribute('src'));
-    imagesGalleryBlock.style.display = 'block';
+    // imagesGalleryBlock.style.display = 'block';
+    imagesGalleryBlock.classList.remove('images-gallery-block_hidden');
     imageElement.src = imagesList[imageIndex];
   }
 }
 
 function closeImageGallery(event) {
   if (event.target.className === 'images-gallery-block__container' || event.target.className === 'images-gallery-block__close') {
-    imagesGalleryBlock.style.display = 'none';
+    // imagesGalleryBlock.style.display = 'none';
+    imagesGalleryBlock.classList.add('images-gallery-block_hidden');
   }
 }
 
 function switchImage(event) {
   if (event.target.className.includes('images-gallery-block__slide-right')) {
-    nextImage(event);
+    event.preventDefault();
+    nextImage();
   } else if (event.target.className.includes('images-gallery-block__slide-left')) {
-    prevImage(event);
+    event.preventDefault();
+    prevImage();
   }
 }
 
-function nextImage(event) {
-  event.preventDefault();
+function nextImage() {
+  // event.preventDefault();
   if (imageIndex === imagesList.length - 1) {
     imageIndex = 0;
   } else {
@@ -39,8 +43,8 @@ function nextImage(event) {
   imageElement.src = imagesList[imageIndex];
 }
 
-function prevImage(event) {
-  event.preventDefault();
+function prevImage() {
+  // event.preventDefault();
   if (imageIndex === 0) {
     imageIndex = imagesList.length - 1;
   } else {
@@ -57,7 +61,20 @@ function handleImageGalleryTouch(event) {
   }
 }
 
+function keyboardGalleryHandler(event) {
+  if (!imagesGalleryBlock.className.includes('images-gallery-block_hidden')) {
+    event.preventDefault();
+    if (event.key === 'ArrowLeft') {
+      prevImage();
+    }
+    if (event.key === 'ArrowRight') {
+      nextImage();
+    }
+  }
+}
+
 document.addEventListener('click', openImageGallery);
 document.addEventListener('click', closeImageGallery);
 document.addEventListener('click', switchImage);
 document.addEventListener('touchstart', handleImageGalleryTouch, {passive: false});
+document.addEventListener('keydown', keyboardGalleryHandler);
