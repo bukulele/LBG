3749deb48b461c88416a0f3aaefac58c9b95401a
bukulele <zonough@gmail.com>
2024-01-09 13:56:34 -0300

    video element added to to watch video block

--- js/video.js
+++ js/video.js
@@ -2,6 +2,10 @@ let videoContainer = document.querySelector('.watch-video-block__video-container
 let videoControlPanel = document.querySelector('.watch-video-block__control-panel');
 let getFullAccessBlock = videoContainer.querySelector('.video-container__get-access');
 let getFullAccessCloseButton = getFullAccessBlock.querySelector('.get-access__close');
+let videoBlockOver =videoContainer.querySelector('.video-container__block-over');
+let videoContainerImage = videoContainer.querySelector('img');
+let videoContainerVideo = videoContainer.querySelector('video');
+let playVideoButton = videoContainer.querySelector('.controls__play-container');
 let videoContainerHeight;
 let windowWidth;
 
@@ -30,3 +34,12 @@ function setControlPanelMinHeight () {
 function closeFullAccessBlock () {
   getFullAccessBlock.style.display = 'none';
 }
+
+function playVideo(event) {
+  videoContainerImage.style.display = 'none';
+  videoBlockOver.style.display = 'none';
+  videoContainerVideo.style.display = 'block';
+  videoContainerVideo.play();
+}
+
+playVideoButton.addEventListener('click', playVideo);
