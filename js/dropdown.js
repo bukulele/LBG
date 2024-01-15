bd22424ca29a0e4f68fce81f0b919361151056ad
bukulele <zonough@gmail.com>
2024-01-15 14:20:09 -0300

    dropdown bugs fixed

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,34 +2,46 @@ let selectorIsOpened = false;
 
 function showSelector(event) {
   if (event.target.className.includes('selector__label-container')) {
-    if (selectorIsOpened) {
-      hideSelector();
-    }
+    // if (selectorIsOpened) {
+    //   hideSelector();
+    // }
     event.stopImmediatePropagation();
     let selectorContainer = event.target.closest('div[data-selector]');
     let dropdown = selectorContainer.querySelector('.dropdown__container');
-    selectorIsOpened = true;
-    // selectorIsOpened = dropdown;
-    dropdown.style.display = 'block';
-    setTimeout(() => {
-      dropdown.style.transform = 'scaleY(1)';
-      dropdown.style.opacity = '1';
-    }, 0);
+    if (dropdown.className.includes('dropdown__container_showed')) {
+      hideSelector();
+    } else {
+      hideSelector();
+      dropdown.classList.remove('dropdown__container_hidden');
+      dropdown.classList.add('dropdown__container_showed');
+      selectorIsOpened = true;
+      turnChevron(dropdown);
+    }
+    // selectorIsOpened = true;
+    // // selectorIsOpened = dropdown;
+    // dropdown.style.display = 'block';
+    // setTimeout(() => {
+    //   dropdown.style.transform = 'scaleY(1)';
+    //   dropdown.style.opacity = '1';
+    // }, 0);
+    // turnChevron(selectorContainer);
   }
 }
 
 function selectOption(event) {
-  if (event.target.className.includes('selector__option')) {
+  if (event.target.className.includes('selector__option') && !selectorIsOpened) {
+    // event.stopImmediatePropagation();
     let selectorContainer = event.target.closest('div[data-selector]');
     let selectorChoice = selectorContainer.querySelector('.selector__choice p');
-    let dropdown = selectorContainer.querySelector('.dropdown__container');
+    // let dropdown = selectorContainer.querySelector('.dropdown__container');
     selectorChoice.innerHTML = event.target.innerText;
-    hideSelector();
+    // hideSelector();
     // hideSelector(selectorIsOpened);
   }
 }
 
 function handleClickOutsideSelector(event) {
+  // console.log(event.target);
   let currentTarget = event.target;
 
   while (typeof currentTarget.className === 'string' && typeof currentTarget.className.includes !== 'undefined' && !currentTarget.className.includes('dropdown__container')) {
@@ -53,17 +65,22 @@ function handleClickOutsideSelector(event) {
 }
 
 function hideSelector() {
+  // console.log('wob');
   let openedDropdowns = document.querySelectorAll('.dropdown__container');
-  openedDropdowns.forEach(element => {
-    let elementStyles = getComputedStyle(element);
-    if (elementStyles.display !== 'none') {
+  openedDropdowns.forEach(dropdown => {
+    let elementStyles = getComputedStyle(dropdown);
+    if (dropdown.className.includes('dropdown__container_showed')) {
+      // let dropdownContainer = element.closest('div[data-selector]');
       // hideSelector(element);
-      element.style.transform = 'scaleY(0.9)';
-      element.style.opacity = '0';
-      setTimeout(() => {
-        element.style.display = 'none';
-        // selectorIsOpened = null;
-      }, 200);
+      dropdown.classList.remove('dropdown__container_showed');
+      dropdown.classList.add('dropdown__container_hidden');
+      turnChevron(dropdown);
+      // element.style.transform = 'scaleY(0.9)';
+      // element.style.opacity = '0';
+      // setTimeout(() => {
+      //   element.style.display = 'none';
+      //   // selectorIsOpened = null;
+      // }, 200);
     }
   });
   // dropdown.style.transform = 'scaleY(0.9)';
@@ -73,6 +90,17 @@ function hideSelector() {
   //   // selectorIsOpened = null;
   // }, 200);
   selectorIsOpened = false;
+}
+
+function turnChevron(dropdown) {
+  console.log('pupupu');
+  let selectorContainer = dropdown.closest('div[data-selector]');
+  let chevron = selectorContainer.querySelector('.selector__chevron');
+  if (dropdown.className.includes('dropdown__container_showed')) {
+    chevron.classList.add('selector__chevron_turned');
+  } else {
+    chevron.classList.remove('selector__chevron_turned');
+  }
 }
 
 document.addEventListener('click', showSelector);
